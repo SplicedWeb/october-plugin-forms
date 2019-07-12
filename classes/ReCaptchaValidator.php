c2b64e63e95445e8aa30da6365d706514efdb0c1
Nathan Smith <nathan@splicedweb.com>
2019-07-13 00:04:49 +1000

    Update ReCaptchaValidator.php
    Allow for use of curl since allow_url_fopen = 0 on many shared hosts.

--- classes/ReCaptchaValidator.php
+++ classes/ReCaptchaValidator.php
@@ -4,18 +4,34 @@
 
     use Request;
     use Martin\Forms\Models\Settings;
-
+	
     class ReCaptchaValidator {
-
-        public function validateReCaptcha($attribute, $value, $parameters) {
+    	
+	public function validateReCaptcha($attribute, $value, $parameters) {
             $secret_key = Settings::get('recaptcha_secret_key');
             $recaptcha  = post('g-recaptcha-response');
             $ip         = Request::getClientIp();
-            $URL        = "https://www.google.com/recaptcha/api/siteverify?secret=$secret_key&response=$recaptcha&remoteip=$ip";
-            $response   = json_decode(file_get_contents($URL), true);
-            return ($response['success'] == true);
+            $data = array(
+            'secret' => $secret_key,
+            'response' => $recaptcha,
+            'remoteip' => $ip
+        	);
+      
+            if (!function_exists('curl_init')){ 
+		        die('CURL is not installed!');
+		    }
+		    $verify = curl_init();
+		    curl_setopt($verify, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+		    curl_setopt($verify, CURLOPT_POST, true);
+		    curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
+		    curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
+		    $response = json_decode(curl_exec($verify), true);
+		    curl_close($verify);
+		    return ($response['success'] == true);
         }
 
+
     }
+    
 
 ?>
